c97bebed9a0cc029eb1e688859ac689a3fae1000
daniel.magalhaes <daniel.magalhaes@farfetch.com>
2022-02-04 12:03:22 +0000

    Arrays vs slices and other stuff

--- arraysVsSlices.go
+++ arraysVsSlices.go
@@ -2,11 +2,45 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
 
 	//Arrays have a fixed limit, they are not resizable
+	// Arrays ar of value type and not of reference type
+	arr := [4]string{"docker", "kuberneted", "terraform"}
+
+	//Declaring an array with the ellipsis, the array lenght is determined by the number of initialized elements
+	// E.g arr := [...]string{"docker", "kuberneted"} -> len(arr) = 2
+
+	fmt.Println("\nTraversing array")
+	for i := 0; i < len(arr); i++ {
+		fmt.Println(arr[i])
+	}
+
+	//Multi dimensional arrays
+	array2d := [3][2]string{
+		{"C#", "C"},
+		{"GO", "Python"},
+		{"C++", "TypeScript"},
+	}
+
+	fmt.Println("\nTraversing 2d array")
+
+	for i := 0; i < len(array2d); i++ {
+		for j := 0; j < len(array2d[0]); j++ {
+			fmt.Println(array2d[i][j])
+		}
+	}
+
+	fmt.Println("\nTraversing 2d array with a for range loop")
+
+	for _, row := range array2d {
+		for _, val := range row {
+			fmt.Println(val)
+		}
+	}
 
 	//Slices are based on arrays, but they are resizable
 	//Slices are passed to functions by reference
@@ -31,13 +65,44 @@ func main() {
 	// When capacity is reached the array is doubled sized e.g. -> 4 -> 8 ->16 ->32
 	mySlice := make([]int, 1, 4)
 
+	fmt.Println("\nIncreesing the capacity of a slice")
 	for i := 1; i < 17; i++ {
 		mySlice = append(mySlice, i)
 		fmt.Printf("\nCapacity is: %d", cap(mySlice))
 	}
 
 	//Appending two slices of the same type(we're appending newSlice integers to mySlice)
+	fmt.Println("\nAppending two slices")
 	newSlice := []int{100, 200, 300}
 	mySlice = append(mySlice, newSlice...)
 	fmt.Println(mySlice)
+
+	// Creating multi-dimensional slice
+	s1 := [][]int{
+		{12, 34},
+		{56, 47},
+		{29, 40},
+		{46, 78},
+	}
+
+	// Accessing multi-dimensional slice
+	fmt.Println("Slice 1 : ", s1)
+
+	// Creating multi-dimensional slice
+	s2 := [][]string{
+		{"Docker", "K8S"},
+		{"Terraform", "AWS"},
+		{"GCP", "Azure"},
+	}
+
+	// Accessing multi-dimensional slice
+	fmt.Println("Slice 2 : ", s2)
+
+	//Sorting slices in ascending order
+	scl1 := []int{400, 600, 100, 300, 500, 200, 900}
+
+	fmt.Println("Is slice sorted? ", sort.IntsAreSorted(scl1))
+	sort.Ints(scl1)
+	fmt.Println("Is slice sorted? ", sort.IntsAreSorted(scl1))
+
 }
--- structs.go
+++ structs.go
@@ -4,26 +4,65 @@ import (
 	"fmt"
 )
 
+type courseMeta struct {
+	Author string
+	Level  string
+	Rating float64
+}
+
 func main() {
 	//Structs are custom data types
 	//Go doesn't have Objects, Classes and inheritance
 
-	type courseMeta struct {
-		Author string
-		Level  string
-		Rating float64
-	}
-
 	//var DockerDeepDive courseMeta
 	//DockerDeepDive := new(courseMeta)
 
-	DockerDeepDive := courseMeta{
-		Author: "Nigel Poulton",
+	dockerDeepDive := courseMeta{
+		Author: "Some Person",
 		Level:  "Intermediate",
 		Rating: 5,
 	}
 
-	fmt.Println(DockerDeepDive)
-	fmt.Println("\nDocker deep dive author is:", DockerDeepDive.Author)
-	fmt.Println("\nDocker deep dive rating is:", DockerDeepDive.Rating)
+	dockerDeepDivePointer := &courseMeta{
+		Author: "Some Person 2",
+		Level:  "Intermediate",
+		Rating: 5,
+	}
+
+	fmt.Println(dockerDeepDive)
+	fmt.Println("\nDocker deep dive author is:", dockerDeepDive.Author)
+	fmt.Println("\nDocker deep dive rating is:", dockerDeepDive.Rating)
+
+	//Modifying by value, nothing happens because an entirely new copy of the struct is created and set to the function
+	modifyValue(dockerDeepDive)
+	fmt.Println("\nDocker deep dive author is:", dockerDeepDive.Author)
+
+	//Modifying by pointer, it actually changes the Author because we are passing a pointer to the reference
+	modifyPointer(&dockerDeepDive)
+	fmt.Println("\nDocker deep dive author is:", dockerDeepDive.Author)
+
+	fmt.Println("\n")
+	fmt.Println((*dockerDeepDivePointer))
+	fmt.Println("\nDocker deep dive author is:", (*dockerDeepDivePointer).Author)
+	fmt.Println("\nDocker deep dive rating is:", (*dockerDeepDivePointer).Rating)
+
+	//Anonymous structure, useful when you want to create a one-time usable structure
+	element := struct {
+		name string
+		from string
+	}{
+		name: "Daniel",
+		from: "Portugal",
+	}
+
+	fmt.Println(element)
+
+}
+
+func modifyValue(c courseMeta) {
+	c.Author = "Modified " + c.Author
+}
+
+func modifyPointer(c *courseMeta) {
+	c.Author = "Modified " + c.Author
 }
